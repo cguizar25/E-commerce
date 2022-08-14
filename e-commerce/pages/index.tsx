97efa37d5100090864db0a909bfa651c2d7dc2d5
Christian Guizar <christianguizar@MacBook-Pro.local>
2022-08-13 21:59:59 -0700

    feat(productsTest): adding test for product function

--- e-commerce/pages/index.tsx
+++ e-commerce/pages/index.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import type { InferGetStaticPropsType } from 'next';
+
+const getStaticProps = async () => {
+  const products = [1, 2, 3];
+
+  return {
+    props: {
+      products
+    },
+    revalidate: 4 * 60 * 60
+  }
+}
+
+export default function Home({
+  products
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div>
-      Testing
+      { products }
     </div>
   )
 }
