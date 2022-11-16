2cf57d7fb9525a14d285f9a195dd9f22e416ba13
Christian Guizar <christianguizar@macbook-pro.lan>
2022-11-16 12:11:33 -0800

    feat(cssPackages): integrated tailwind and post css into project

--- e-commerce/next.config.js
+++ e-commerce/next.config.js
@@ -2,11 +2,12 @@ const { withFrameworkConfig } = require("./framework/common/config")
 
 module.exports = withFrameworkConfig({
   framework: {
-    name: "shopify_local"
+    name: process.env.NEXT_PUBLIC_FRAMEWORK
   },
   i18n: {
     locales: ["en-US", "es"],
     defaultLocale: "en-US"
   }
 })
+
 console.log("next.config.js", JSON.stringify(module.exports, null, 2))
