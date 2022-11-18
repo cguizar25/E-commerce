7f37987cb4c04bfe627736af4fd2a2bae3be93bf
Christian Guizar <christianguizar@macbook-pro.lan>
2022-11-17 19:09:15 -0800

    style(postCSS): generated postcss output via command

--- e-commerce/tailwind.config.js
+++ e-commerce/tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  purge: [
+  content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}'
   ],
-  darkMode: false,
+  media: false,
   theme: {
     extend: {},
   },
