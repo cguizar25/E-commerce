b1b1397ae789b26105dd715e76e604c744907055
Christian <c-guizar@hotmail.com>
2023-02-10 19:39:27 -0800

    feat(memo): memoized valu variablein context

--- e-commerce/components/common/Layout/Layout.tsx
+++ e-commerce/components/common/Layout/Layout.tsx
@@ -2,23 +2,25 @@
 import { FC } from "react";
 import s from "./Layout.module.css";
 import { Footer, Navbar } from "@components/common";
+import { Sidebar } from "@components/ui";
 import { CartSidebar } from "@components/cart";
 import { useUI } from "@components/ui/context";
 
-
 const Layout: FC = ({c}) => {
   const { isSidebarOpen, closeSidebar } = useUI();
-  console.log("Re-rendering LAYOUT");
+
   return (
     <div className={s.root}>
       <Navbar />
-      <Sidebar isOpen={ui.isSideBarOpen} onClose={closeSidebar}>
+      <Sidebar
+        onClose={closeSidebar}
+        isOpen={isSidebarOpen}>
         <CartSidebar />
       </Sidebar>
-        <main className="fit">
-          {c}
-        </main>
-        <Footer />
+      <main className="fit">
+        { c }
+      </main>
+      <Footer />
     </div>
   )
 }
