7d0a12efa0e160b85dbf800241f2a583d57498fb
Christian <c-guizar@hotmail.com>
2023-02-22 18:43:57 -0800

    feat(normalized): took out placeholder for un normalized code and normalized it accordingly

--- e-commerce/framework/shopify/utils/normalize.ts
+++ e-commerce/framework/shopify/utils/normalize.ts
@@ -24,11 +24,26 @@ const normalizeProductOption = ({
   name: displayName
 }: ProductOption) => {
 
-  console.log("ID ", id)
-  console.log("NAME ", displayName)
-  console.log("VALUES ", values)
+  const normalized = {
+    id,
+    displayName,
+    values: values.map(value => {
+      let output: any = {
+        label: value
+      }
 
-  return {}
+      if (displayName.match(/colou?r/gi)) {
+        output = {
+          ...output,
+          hexColor: value
+        }
+      }
+
+      return output;
+    })
+  }
+
+  return normalized
 }
 
 export const normalizeProduct = (productNode: ShopifyProduct): Product => {
