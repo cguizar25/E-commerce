eae6d45eb4c5bcc4f85c2bf402075a30335c21a5
Christian <c-guizar@hotmail.com>
2023-02-28 14:33:37 -0800

    style(keenSlider): added keen slider stylings to app

--- e-commerce/framework/shopify/product/ProductSlider/ProductSlider.tsx
+++ e-commerce/framework/shopify/product/ProductSlider/ProductSlider.tsx
@@ -1,12 +1,20 @@
-import { FC } from "react";
+import { FC, Children, isValidElement } from "react";
 import s from "./ProductSlider.module.css";
 
 const ProductSlider: FC = ({children}) => {
 
   return (
     <div className={s.root}>
-      <div className="h-full transition-opacity">
-        {children}
+      <div className="keen-slider h-full transition-opacity">
+        { Children.map(children, child => {
+          if (isValidElement(child)) {
+
+            return React.cloneElement(child, {className: "keen-slider__slide"})
+          }
+
+          return child
+        }
+        )}
       </div>
     </div>
   )
--- e-commerce/pages/_app.tsx
+++ e-commerce/pages/_app.tsx
@@ -1,14 +1,13 @@
 import "@assets/main.css";
+import 'keen-slider/keen-slider.min.css';
 import { AppProps } from "next/app";
 import { FC } from "react";
 import { UIProvider } from "@components/ui/context";
 
-const Noop: FC = (children) => <>{children}</>;
-
-
-function MyApp({Component, pageProps}: any & {Component: {Layout: FC}}) {
+const Noop: FC = ({children}) => <>{children}</>
+function MyApp({Component, pageProps}: AppProps & {Component: {Layout: FC}}) {
   const Layout = Component.Layout ?? Noop
-  
+
   return (
     <UIProvider>
       <Layout>
@@ -17,5 +16,4 @@ function MyApp({Component, pageProps}: any & {Component: {Layout: FC}}) {
     </UIProvider>
   )
 }
-
 export default MyApp;
