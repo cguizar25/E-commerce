31d00a9b1346319be646812ea5490499fb3d9eef
Christian <c-guizar@hotmail.com>
2023-04-06 23:05:23 -0700

    fix(type): addressed issue with mutation type and apihooks

--- e-commerce/framework/common/utils/use-hook.ts
+++ e-commerce/framework/common/utils/use-hook.ts
@@ -1,6 +1,6 @@
 import { useApiProvider } from "@common";
-import { ApiHooks } from "@common/types/api";
-
+import { ApiHooks } from "@common/types/hooks";
+import { MutationHook } from "@common/types/hooks";
 
 export const useHook = (fn: (apiHooks: ApiHooks) => any) => {
   const { hooks } = useApiProvider();
