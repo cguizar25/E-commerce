b09fee21bf0c0aeb95ea4719f38a228428a91f4a
Christian <c-guizar@hotmail.com>
2023-04-10 15:19:52 -0700

    feat(cookie): added cookie to hold checkout id

--- e-commerce/framework/shopify/cart/use-cart.ts
+++ e-commerce/framework/shopify/cart/use-cart.ts
@@ -6,7 +6,11 @@ export const handler = {
   fetchOptions: {
     query: "query { hello }"
   },
-  async fetcher({fetch, options}: any) {
+  async fetcher({
+    fetch,
+    options,
+    input: { checkoutId }
+  }: any) {
     const data = await fetch({...options})
     return { data }
   },
