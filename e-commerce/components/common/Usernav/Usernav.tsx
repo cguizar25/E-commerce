122954d9ccfad704c6012b14e91d2c5c2232c147
Christian <c-guizar@hotmail.com>
2023-04-20 20:51:38 -0700

    chore(swr): improved swr use in usernav

--- e-commerce/components/common/Usernav/Usernav.tsx
+++ e-commerce/components/common/Usernav/Usernav.tsx
@@ -3,9 +3,12 @@ import s from "./Usernav.module.css";
 import Link from "next/link";
 import { Items as Cart, Favorite } from "@components/symbols";
 import { useUI } from "@components/ui/context";
+import useCart from "@common/cart/use-cart";
 
 const Usernav: FC = () => {
   const { openSidebar } = useUI();
+  const { data } = useCart();
+  debugger;
 
   return (
     <nav>
