2fa12974b6f4bed149983cd1b3dab2c706ddc31a
Christian <c-guizar@hotmail.com>
2023-05-23 22:15:28 -0700

    feat(remove): added component to remove items

---
+++ e-commerce/framework/common/cart/use-remove-item.tsx
@@ -0,0 +1,14 @@
+import {
+    useHook,
+    useMutationHook
+  } from "@common/utils/use-hook"
+  
+  const useRemoveItem = () => {
+    const hook = useHook((hooks) => {
+      return hooks.cart.useRemoveItem
+    })
+  
+    return useMutationHook({...hook})()
+  }
+  
+  export default useRemoveItem;
